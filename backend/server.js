312bb3f0718ab24fc76f13714ebef5326403c1f5
Hemant Bisht <hmntbst2210@gmail.com>
2024-11-22 11:24:02 +0530

    cleanup

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,6 @@ app.use(express.json({ limit: "10mb" })); // to parse req.body... limit size for
 app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
 
-app.get("/test", (req, res) => {
-  res.send("Backend is working!");
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
